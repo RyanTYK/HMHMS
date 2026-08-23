235ee3320ad7e9a3c16ca675ce0546f4fa6a7808
RyanTYK <72983493+RyanTYK@users.noreply.github.com>
2026-08-23 23:18:52 +0800

    Replace Select All/Deselect All button icon with a double-checkmark

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -240,8 +240,8 @@
                 :title="allFilteredSelected ? 'Deselect All' : 'Select All'"
               >
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
-                  <path v-if="!allFilteredSelected" stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-                  <path v-else stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 12l4 4l9 -9" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2 12l4 4m4 -4l4 -4" />
                 </svg>
               </button>
               <button 
